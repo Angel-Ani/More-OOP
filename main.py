a7eafb6d3e85d3e9ef9a061423ea31dd030577db
Angel-Ani <Angel.Ani@laetottenham.ac.uk>
2024-11-11 12:26:51 +0000

    Started

---
+++ main.py
@@ -0,0 +1,26 @@
+class Employee: 
+    def __init__(self,name,role, salary, DoB):
+        self.name = name
+        self.role = role
+        self.salary = salary
+        self.DoB = DoB
+
+    def increase_salary(self):
+        i_salary =     
+
+    def caluclate_age(self):
+
+
+class Manager: 
+     def __init__(self, name, role, salary, DoB, bonus):
+        self.name = name
+        self.role = role
+        self.salary = salary
+        self.DoB = DoB
+        self.bonus = bonus 
+
+    def increase_salary(self):
+        i_salary =     
+
+    def caluclate_age(self):
+        
